6a4124a6111c557e5e330274d77094c982293c6b
Brice Lin <bricejlin@gmail.com>
2015-11-06 09:27:29 -0500

    Revert "fix(package.json): add missing package"
    This reverts commit 210163d04429bfb4266eea2c871e48126c87167e.
    
    Revert "bump to 1.0.4"
    
    This reverts commit 9f92d56e26215c497ce6fc33284eeb9ea341086c.
    
    Revert "Change code to es6"
    
    This reverts commit bf490186bbaf9c9a2c7b26dc3f4e257694627a48.
    
    Revert "bump to 1.0.3"
    
    This reverts commit 7472eed13dd3923199791f33f09b07d3484cce84.
    
    Revert "fix(package.json): add missing package"
    
    This reverts commit 7a3d7e5afc73ff3e9fae543460f8a9f46131e5cd.
    
    Bump reverts

--- index.js
+++ index.js
@@ -1,1 +1,95 @@
-module.exports = require('./dist/holy-bible');
+'use strict';
+
+var Promise = require('es6-promise').Promise;
+var zeroFill = require('zero-fill');
+
+var BCVParser =
+  require('bible-passage-reference-parser/js/en_bcv_parser').bcv_parser;
+var bcv = new BCVParser();
+
+var BOOK_TO_INDEX = require('./indexes/book-index-map');
+var VERSE_INDEX = require('./indexes/verse-index-map');
+
+var BIBLES = {
+  'asv': require('./bibles/asv'),
+  'kjv': require('./bibles/kjv')
+};
+
+module.exports = (function () {
+  var bible = {};
+
+  /**
+   * Retrieve bible passages
+   *
+   * @param {String} psg - bible book/chapter/verse
+   * @param {String} ver - bible version
+   * @return {Promise} returns String passage
+   */
+  bible.get = function (psg, ver) {
+    if (typeof psg !== 'string') { throw new TypeError('Expected a string'); }
+
+    var self = this;
+    var v = ver || 'asv';
+    // clean/normalize psg to bcv object
+    var psgBCV = bcv.parse(psg).parsed_entities()[0].entities[0];
+
+    if (!psgBCV) { throw new Error('Bad bible passage input'); }
+
+    return new Promise(function (res, rej) {
+      // parse psg into index ranges
+      var start = self._bcvToInd(psgBCV.start);
+      var end = self._bcvToInd(psgBCV.end);
+      var psgOsis = psgBCV.osis;
+      var text = (end - start === 0) ? self._getVerses(BIBLES[v], start)
+                                     : self._getVerses(BIBLES[v], start, end);
+
+      if (text) {
+        var data = { version: v, passage: psgOsis, text: text };
+        res(data);
+      } else {
+        rej(Error('Ahhhhh!!! Nooo!!!'));
+      }
+    });
+  };
+
+  /**
+   * Convert book, chapter, verse object into verse index
+   *
+   * @param {Object} psg - bible book/chapter/verse obj
+   * @return {String} verse index (ie: 01001001)
+   */
+  bible._bcvToInd = function (psg) {
+    var book = BOOK_TO_INDEX[psg.b].b;
+    var b = zeroFill(2, book);
+    var c = zeroFill(3, psg.c);
+    var v = zeroFill(3, psg.v);
+
+    return b + c + v;
+  };
+
+  /**
+   * Convert book, chapter, verse object into verse index
+   *
+   * @param {Object} bVersions - bible version object
+   * @param {String} start - start index
+   * @param {String} end - end index
+   * @return {String} bible passage
+   */
+  bible._getVerses = function (bVersions, start, end) {
+    var s = VERSE_INDEX[start];
+    var e = VERSE_INDEX[end];
+    var arr = [];
+
+    // Return single verse if no end
+    if (!e) { return bVersions[s]; }
+
+    for (var i = Number(s), len = Number(e);
+           i < len + 1; i++) {
+      arr.push(bVersions[i]);
+    }
+
+    return arr.join(' ');
+  };
+
+  return bible;
+})();
